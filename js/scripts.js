5550fc95a3a798cb78e5e1d7c9d2230c20433907
tasiakins <tasiapele@gmail.com>
2014-05-12 13:46:53 -0700

    video

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 // My Scripts
 $(document).foundation();
+
+//bigvideo
+
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://tasiakins.github.io/doh/oceans.mp4');
+});
